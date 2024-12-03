20e3e39f71732deaed19e1ebc55b54b71032996f
arthursquirtle <arthursquirtle@outlook.com>
2024-12-02 20:25:21 -0500

    p1finish

---
+++ adventOfCode/p1.py
@@ -0,0 +1,30 @@
+#with open('adventOfCode/p1input.txt', 'r') as file:
+with open('adventOfCode/p1input.txt', 'r') as file:
+    count = 0
+    lList = []
+    rList = []
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        #print(f"Line{count}: {line.strip()}")
+        #print(f"Line{count}: {line.split()}")
+        splitVal = line.split()
+        lVal = int(splitVal[0])
+        rVal = int(splitVal[1])
+        #print (lVal, rVal, lVal < rVal)
+        lList.append(lVal)
+        rList.append(rVal)
+    lList.sort()
+    rList.sort()
+    #print (lList)
+    #print (rList)
+    for i in range(0,len(lList)):
+        #print(rList[i])
+        diff = lList[i]- rList[i]
+        #print(abs(diff),'\n')
+        count += abs(diff)
+        print(lList[i],rList[i], abs(diff))
+        print(count)
+    #for val in a:
+    #print(val)
